0b36dd154fc0689b3b9ff897a20176b95175facd
Ansar Memon <ansar.memon@hotmail.com>
2020-04-04 17:01:08 +0400

    Added header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
+    <>
+    <Header />
     <Switch>
       <Route path="/shop" component={ShopPage} />
       <Route exact path="/" component={HomePage} />
     </Switch>
+    </>
   );
 }
 
