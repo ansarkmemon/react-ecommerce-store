a8e41122c4e8a6e9f7491f25634ae3e51ba170f8
Ansar Memon <ansar.memon@hotmail.com>
2020-04-07 13:09:20 +0400

    Added sign up with email and password

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -17,8 +17,19 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({ currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }})
+        });
+      } else {
+        this.setState({ currentUser: null });
+      }
     })
   }
 
