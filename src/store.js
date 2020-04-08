4df3d690d1df3c461143d2f32ee5598b5e34931d
Ansar Memon <ansar.memon@hotmail.com>
2020-04-08 17:46:42 +0400

    added persistence with local storage for cart items using redux-select

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from 'redux-persist';
 import logger from "redux-logger";
 
 import rootReducer from './reducers/root-reducer';
 
 const middlewares = [logger];
 
-export default createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+export const persistor = persistStore(store);
+
+export default store;
