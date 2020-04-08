239ce9639408b1f7ee04c03c1315c05cd1fc882e
Ansar Memon <ansar.memon@hotmail.com>
2020-04-08 18:22:47 +0400

    added directory and shop collection to redux store

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { SHOP_DATA } from '../../mockData'
+import { connect } from 'react-redux'
+
+import { selectShopCollection } from '../../selectors/shop.selectors';
 import PreviewCollection from '../../components/preview-collection/preview-collection.component'
 
-const ShopPage = () => {
+const ShopPage = ({ shop }) => {
   return (
     <div className="shop-page">
-      { SHOP_DATA.map(collection => (
+      { shop.map(collection => (
         <PreviewCollection key={collection.id} data={collection} />
       ))}
     </div>
   )
 }
-
-export default ShopPage
+const mapStateToProps = state => ({
+  shop: selectShopCollection(state)
+})
+export default connect(mapStateToProps)(ShopPage)
---
+++ src/selectors/shop.selectors.js
@@ -0,0 +1,5 @@
+import { createSelector } from 'reselect';
+
+export const selectShop = state => state.shop;
+
+export const selectShopCollection = createSelector([selectShop], shop => shop.collections);
