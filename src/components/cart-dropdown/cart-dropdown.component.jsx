2e903bf4c2969b8291a652fde076e55ad9497d90
Ansar Memon <ansar.memon@hotmail.com>
2020-04-08 11:35:41 +0400

    added items to the cart with styling

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import CustomButton from '../custom-button/custom-button.component';
 import { connect } from 'react-redux';
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = (props) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        { props.cartItems.map(item => <p key={item.id}>{item.name}</p>)}
+        { props.cartItems.map(item => <CartItem key={item.id} item={item} />)}
       </div>
       <CustomButton>GO TO CHECKOUT</CustomButton> 
     </div>
